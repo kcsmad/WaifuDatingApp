615177c877d9ad3098d3d115ef50e127c3b95da4
Kevin Maduro <nivekzin@gmail.com>
2018-03-03 15:56:29 -0300

    Section 3, Lecture 28

--- Startup.cs
+++ Startup.cs
@@ -30,6 +30,7 @@ namespace WaifuDatingApp.API
                 ));
             services.AddMvc();
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
