f24a812107fb7f36cbb8a3cb0c3838c8e7d9845b
Kevin Maduro <nivekzin@gmail.com>
2018-03-03 16:04:25 -0300

    Section 3, Lecture 29

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WaifuDatingApp.API.Data;
+using WaifuDatingApp.API.Models;
+
+namespace WaifuDatingApp.API.Controllers
+{
+    [Route("api/[controller])")]
+    public class AuthController : Controller
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            //Validate request;
+
+            username = username.ToLower();
+
+            if (await _repo.UserExists(username))
+                return BadRequest("Username is already taken");
+
+            var userToCreate = new User
+            {
+                Username = username
+            };
+
+            var createUser = await _repo.Register(userToCreate, password);
+
+            return StatusCode(201);
+        }
+    }
+}
