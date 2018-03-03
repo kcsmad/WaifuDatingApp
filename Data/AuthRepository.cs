49dec67f8f6af61a69ad1d68619d8ec5ec6ca661
Kevin Maduro <nivekzin@gmail.com>
2018-03-03 15:52:58 -0300

    Section 3, Lecture 27

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WaifuDatingApp.API.Models;
+
+namespace WaifuDatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> LoginAsync(string username, string pass)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+
+            if(user == null)
+                return null;
+
+            if (!VerifyPasswordHash(pass, user.PasswordHash, user.PasswordSalt))
+                return null;
+
+            return user;
+        }
+
+        private bool VerifyPasswordHash(string pass, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(pass));
+                for(int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i]) return false;
+                }
+            }
+
+            return true;
+        }
+
+        public async Task<User> Register(User user, string pass)
+        {
+            byte[] passHash, passSalt;
+            CreatePasswordHash(pass, out passHash, out passSalt);
+
+            user.PasswordHash = passHash;
+            user.PasswordSalt = passSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+        private void CreatePasswordHash(string pass, out byte[] passHash, out byte[] passSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passSalt = hmac.Key;
+                passHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(pass));
+            }
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            if (await _context.Users.AnyAsync(x => x.Username == username))
+                return true;
+
+            return false;
+        }
+
+
+    }
+}
--- Data/IAuthRepository.cs
+++ Data/IAuthRepository.cs
@@ -7,7 +7,7 @@ namespace WaifuDatingApp.API.Data
     public interface IAuthRepository
     {
         Task<User> Register(User user, string pass);
-        Task<User> Login(string username, string pass);
+        Task<User> LoginAsync(string username, string pass);
         Task<bool> UserExists(string username);
     }
 }
