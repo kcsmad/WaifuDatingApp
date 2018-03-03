239d0773b2f697f831e2f97cfde13ad5b75c26e0
Kevin Maduro <nivekzin@gmail.com>
2018-03-03 15:35:11 -0300

    Section 3, Lecture 26

---
+++ Data/IAuthRepository.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Threading.Tasks;
+using WaifuDatingApp.API.Models;
+
+namespace WaifuDatingApp.API.Data
+{
+    public interface IAuthRepository
+    {
+        Task<User> Register(User user, string pass);
+        Task<User> Login(string username, string pass);
+        Task<bool> UserExists(string username);
+    }
+}
