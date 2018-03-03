1e676b31bc34796ff1deba3cac51671fa295252f
Kevin Maduro <nivekzin@gmail.com>
2018-03-03 12:29:35 -0300

    Initial Commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WaifuDatingApp.API.Models;
+
+namespace WaifuDatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
