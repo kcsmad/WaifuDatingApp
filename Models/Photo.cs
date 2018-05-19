b4a35b34945ed23de7b822d0b1dc0f1959328c36
Kevin Maduro <nivekzin@gmail.com>
2018-05-19 19:20:12 -0300

    Section 8 - Lecture 69

--- Models/Photo.cs
+++ Models/Photo.cs
@@ -8,6 +8,8 @@ namespace WaifuDatingApp.API.Models
         public string Url { get; set; }
         public string Description { get; set; }
         public DateTime DateAdded { get; set; }
-        public bool MainPhoto { get; set; }
+        public bool IsMain { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; }
     }
 }
